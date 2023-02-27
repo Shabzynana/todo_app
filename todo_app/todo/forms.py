cfeb33e11293408fc5f1f3cd3bc204d89cd64194
Adebiyi Oluwafemi <horlarmihleykan10@gmail.com>
2023-02-27 23:52:23 +0100

    new

--- todo_app/todo/forms.py
+++ todo_app/todo/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.fields import DateField
+from wtforms.fields.html5 import DateField
 
 from wtforms.validators import DataRequired
 
