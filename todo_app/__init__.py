03097d8fb7c387f120e59313a253c2de7c305ead
MAC <horlarmihleykan10@gmail.com>
2023-08-04 00:13:05 +0100

    new

--- todo_app/__init__.py
+++ todo_app/__init__.py
@@ -39,8 +39,8 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['TESTING'] = False
-app.config['MAIL_USERNAME'] = ''
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USERNAME'] = os.environ.get("USER_DB")
+app.config['MAIL_PASSWORD'] = os.environ.get("PASSWORD_DB")
 
 
 mail = Mail(app)
